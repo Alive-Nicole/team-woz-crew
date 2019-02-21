62306e887a6917a770d4220e264a4b06afbaa205
Lah Ntumsi <lah_ntumsi2000@yahoo.com>
2019-02-21 09:22:23 +0300

    front end updates

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import NavBar from './NavBar/NavBar';
+import Users from './Users/Users';
+import User from './User/User';
+import {Route} from 'react-router-dom';
+import NewUser from './NewUser/NewUser';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <NavBar/>
+        <Route exact path ='/' component={Users}/>
+        <Route exact path ='/user/:userId' component={User}/>  
+        <Route exact path = '/'  component={NewUser}/>
       </div>
     );
   }
---
+++ front-end/src/Users/Users.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: null,
+    };
+  }
+
+  async componentDidMount() {
+    const users = (await axios.get('http://localhost:8081/')).data;
+    this.setState({
+      users,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+        <Link to="/new-User">
+            <div className="card text-white bg-secondary mb-3">
+              <div className="card-header">Want to be part of the developer's companion, sign up here!</div>
+              <div className="card-body">
+                <h4 className="card-title">+ </h4>
+                <p className="card-text">Don't worry. we have more to offer once you sign up!</p>
+              </div>
+            </div>
+          </Link>
+          {this.state.questions === null && <p>Loading Users...</p>}
+          {
+            this.state.users && this.state.users.map(user => (
+              <div key={user.id} className="col-sm-12 col-md-4 col-lg-3">
+                <Link to={`/user/${user.id}`}>
+                  <div className="card text-white bg-success mb-3">
+                    <div className="card-header">Users: {user.photo}</div>
+                    <div className="card-body">
+                      <h4 className="card-title">{user.userName}</h4>
+                      <p className="card-text">{user.firstName}, { user.lastName}</p>
+                      <p className="card-text">{user.phone}</p>
+                      <p className="card-text">{user.email}</p>
+                      <p className="card-text">{user.linkedIn}</p>
+                      <p className="card-text">{user.gitHub}</p>
+                      <p className="card-text">{user.languages.length}</p>
+                      <p className="card-text">{user.technologies.length}</p>
+                      <p className="card-text">{user.interests.length}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Users;
